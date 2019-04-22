832678f99b20bc20ae9a84f1d43d061d87f11091
Ethan Vann <Superevevev@gmail.com>
2019-04-22 10:07:10 -0400

    update to use flattened array
    meh its alright test cases pass but more could be added

--- bowlingScore.java
+++ bowlingScore.java
@@ -1,40 +1,34 @@
-public class bowlingScore {
-    public static int bowlingCalc(int[][] scores) {
+public class BowlingScore {
+    public static int bowlingCalc(int[] scores) {
         int totalScore = 0;
+        boolean first = true;
         for (int i = 0; i < scores.length; i++) {
-            //code for handling the last frame || adds the total of all 3 rolls
-            if (i == 9) {
-                totalScore += scores[i][0];
-                totalScore += scores[i][1];
-                totalScore += scores[i][1];
-                //code for handling if the player scores a strike || adds the current frame rolls plus the next two rolls
-            } else if (scores[i][0] == 10 || scores[i][1] == 10) {
-                totalScore += scores[i][0];
-                totalScore += scores[i][1];
-                //handles if the next roll was a strike
-                if (scores[i + 1][0] == 10) {
-                    if (i != 8) {
-                        totalScore += scores[i + 1][0];
-                        totalScore += scores[i + 2][0];
-                        //handles if the next frame is the second last frame and a strike was the  first roll
+            if (i == 18 || i == 19 || i == 20) {
+                totalScore += scores[i];
+            } else if (first) {
+                first = false;
+                if (scores[i] == 10) {
+                    first = true;
+                    totalScore += 10;
+                    if (scores[i + 2] == 10 && i != 16) {
+                        totalScore += scores[i + 2];
+                        totalScore += scores[i + 4];
                     } else {
-                        totalScore += scores[i + 1][0];
-                        totalScore += scores[i + 1][1];
+                        totalScore += scores[i + 2];
+                        totalScore += scores[i + 3];
                     }
+                    i += 1;
                 } else {
-                    //handles if you didnt get a strike on the next frame
-                    totalScore += scores[i + 1][0];
-                    totalScore += scores[i + 1][1];
+                    totalScore += scores[i];
                 }
-                //code for handling if the player scores a spare || adds the current frame rolls plus the next roll
-            } else if (scores[i][0] + scores[i][1] == 10) {
-                totalScore += scores[i + 1][0];
-                totalScore += scores[i][0];
-                totalScore += scores[i][1];
-                //code for handling neither a strike nor a spare adds only the current frame rolls
             } else {
-                totalScore += scores[i][0];
-                totalScore += scores[i][1];
+                first = true;
+                if (scores[i] + scores[i - 1] == 10) {
+                    totalScore += scores[i];
+                    totalScore += scores[i + 1];
+                } else {
+                    totalScore += scores[i];
+                }
             }
         }
         return totalScore;
@@ -42,17 +36,11 @@ public class bowlingScore {
 
     public static void main(String[] args) {
         //frames
-        int[] one = {10, 0};
-        int[] two = {10, 0};
-        int[] three = {10, 0};
-        int[] four = {5, 5};
-        int[] five = {10, 0};
-        int[] six = {10, 0};
-        int[] seven = {10, 0};
-        int[] eight = {10, 0};
-        int[] nine = {10, 0};
-        int[] ten = {10, 10, 10};
-        int[][] scores = {one, two, three, four, five, six, seven, eight, nine, ten};
-        System.out.println(bowlingCalc(scores));
+        int[] scores = {10, 0, 10, 0, 10, 0, 10, 0, 5, 5, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10, 10};
+        int[] scores2 = {1, 9, 3, 7, 2, 8, 5, 5, 5, 5, 2, 8, 3, 7, 9, 1, 6, 4, 4, 6, 1};
+        int[] scores3 = {2, 7, 3, 4, 2, 7, 4, 6, 10, 0, 10, 0, 2, 7, 4, 6, 1, 9, 4, 2, 0};
+        System.out.println("checking all strikes answer should be 300, answer: " + bowlingCalc(scores));
+        System.out.println("checking all spares answer should be 140, answer: " + bowlingCalc(scores2));
+        System.out.println("checking random rolls 1 answer should be 126, answer: " + bowlingCalc(scores3));
     }
 }
