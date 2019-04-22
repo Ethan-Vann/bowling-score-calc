bc342e1d24817b3a3488ff4b6e598941c0f51408
Ethan Vann <Superevevev@gmail.com>
2019-04-22 10:14:02 -0400

    Add files via upload

--- BowlingScore.java
+++ BowlingScore.java
@@ -1,31 +1,45 @@
 public class BowlingScore {
     public static int bowlingCalc(int[] scores) {
         int totalScore = 0;
+        //first used to keep track of whether this is the first roll of the turn
         boolean first = true;
         for (int i = 0; i < scores.length; i++) {
+            //special handling for the last 3 rolls
             if (i == 18 || i == 19 || i == 20) {
                 totalScore += scores[i];
+                //if this is the first roll
             } else if (first) {
+                //set first to false to handle the next roll
                 first = false;
+                //handling for a strike
                 if (scores[i] == 10) {
+                    //set first to true because strikes skip a roll
                     first = true;
+                    //add the 10 points from a strike
                     totalScore += 10;
+                    //if the next roll is also a strike add the first roll after it
                     if (scores[i + 2] == 10 && i != 16) {
                         totalScore += scores[i + 2];
                         totalScore += scores[i + 4];
+                        //if the next roll isn't a strike simple add both of the next turn rolls to the total
                     } else {
                         totalScore += scores[i + 2];
                         totalScore += scores[i + 3];
                     }
                     i += 1;
+                    //if its not a strike simple add the result of this role
                 } else {
                     totalScore += scores[i];
                 }
+                //handling for the second roll of a turn
             } else {
+                //set first back to true to get ready to handle the next roll
                 first = true;
+                //handling of spares add the next roll as well, no special cases
                 if (scores[i] + scores[i - 1] == 10) {
                     totalScore += scores[i];
                     totalScore += scores[i + 1];
+                    //handling for no spare, just add the result of the roll
                 } else {
                     totalScore += scores[i];
                 }
